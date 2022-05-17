039fdd85110293728e13bb57596b3ae612e5c4eb
Simon Doba <simon.dp.doba@campus.tu-berlin.de>
2022-05-17 03:07:35 +0200

    [Changes] formated other files based on eslint config

--- components/Title.tsx
+++ components/Title.tsx
@@ -1,8 +1,8 @@
-import { Pressable, StyleSheet, Text, View } from "react-native";
-import { AntDesign } from "@expo/vector-icons";
-import { PRIMARY } from "../colors";
-import React from "react";
-import { NavigationProp, useNavigation } from "@react-navigation/native";
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+import { PRIMARY } from '../colors';
+import React from 'react';
+import { NavigationProp, useNavigation } from '@react-navigation/native';
 
 type Props = {
   text: string;
@@ -17,8 +17,8 @@ export default function Title({ text, color, Icon, back }: Props) {
   return (
     <View style={[styles.container, { backgroundColor: color ?? PRIMARY }]}>
       {back && navigation.canGoBack() && (
-        <Pressable style={styles.backButton} onPress={() => navigation.goBack()}>
-          <AntDesign name="left" size={30} color="black" />
+        <Pressable onPress={() => navigation.goBack()} style={styles.backButton}>
+          <AntDesign color="black" name="left" size={30} />
           <Text style={{ left: -5, fontSize: 12 }}>Zurück</Text>
         </Pressable>
       )}
@@ -34,23 +34,23 @@ export default function Title({ text, color, Icon, back }: Props) {
 
 const styles = StyleSheet.create({
   backButton: {
-    position: "absolute",
+    position: 'absolute',
     top: 15,
     left: 15,
-    flexDirection: "row",
-    alignItems: "center",
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   container: {
     flexGrow: 0,
     flexShrink: 1,
-    flexBasis: "30%",
-    justifyContent: "center",
-    alignItems: "center",
+    flexBasis: '30%',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   text: {
     fontSize: 30,
-    fontWeight: "bold",
-    textAlign: "center",
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
   icon: {
     marginTop: 20,
