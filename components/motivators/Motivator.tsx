b48e96de760c6f3b632cf3f19864925797ffa5e0
Jonah Balshai <jonahbalshai@gmail.com>
2022-05-18 18:11:34 +0200

    more work on the motivator creation

--- components/motivators/Motivator.tsx
+++ components/motivators/Motivator.tsx
@@ -1,20 +1,24 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { Image, StyleSheet, View, Text, ImageSourcePropType } from "react-native";
 
 type Props = {
     title: string;
     //TODO: icon  
-    icon?: string 
+    icon: ImageSourcePropType 
     description: string;
 }
 
 
-export default function Motivator({title, description}: Props) {
+export default function Motivator({title, description, icon}: Props) {
     return (
         <View style={styles.container}>
-            <Text style={styles.titletext}>{title}</Text>
-            <Text>Description here</Text>
-            <Text style={styles.descriptiontext}>{description}</Text>
+            <View style={styles.textcontainer}>
+                <Text style={styles.titletext}>{title}</Text>
+            </View>
+            <Image source={icon} style={styles.icon}></Image>
+            <View style={styles.textcontainer}>
+                <Text style={styles.descriptiontext}>{description}</Text>
+            </View>
         </View>
     );
 }
@@ -33,18 +37,16 @@ const styles = StyleSheet.create({
         flex: 1,
         flexGrow: 0,
         flexShrink: 1,
-        flexBasis: 150,
         justifyContent: "center"
     },
     container: {
         flexGrow: 0,
         flexShrink: 1,
-        flexBasis: "30%",
         justifyContent: "center",
         alignItems: "center",
     },
     icon: {
-        width: 64,
-        height: 64,
+        width: 128,
+        height: 128,
     }
 })
--- components/motivators/MotivatorCreator.tsx
+++ components/motivators/MotivatorCreator.tsx
@@ -1,54 +1,84 @@
 import React from "react";
-import { Text, StyleSheet, View } from "react-native";
+import { Text, StyleSheet, View, Pressable } from "react-native";
 import { TERTIARY, MOTIVATOR } from "../../colors";
+import { AntDesign } from "@expo/vector-icons";
 import Motivator from "./Motivator";
 import Title from "../Title";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function CreateMotivator() {
+    console.log("TEST");
     return (
       <>
         <Title text="Neue Starkmacher"/>
-        <View style={motivatorStyles.container}>
-          <View style={motivatorStyles.textContainer}>
-            <Text style={motivatorStyles.text}>Welche neuen Starkmacher möchtest du heute ausprobieren?</Text>
+        <View style={styles.container}>
+          <View style={styles.textContainer}>
+            <Text style={styles.text}>Welche neuen Starkmacher möchtest du heute ausprobieren?</Text>
           </View>
-            <Motivator title="Test" description="Description"/>
+          <View style={styles.motivatorContainer}>
+            <View style={styles.arrowContainer}>
+              <AntDesign name="left" size={48} />
+            </View>
+            <Motivator icon={require("../../assets/reframeicon.png")} title="Reframing" description="Beim Reframing geht es darum, deine eigene Einschätzung der Situation zu überprüfen und ggf. zu einer anderen Interpretation zu kommen."/>
+            <View style={styles.arrowContainer}>
+              <AntDesign name="right" size={48}/>
+            </View>
+          </View>
         </View>
       </>
     );
 }
 
-const motivatorStyles = StyleSheet.create({
+const styles = StyleSheet.create({
     container: {
         flex: 1,
         flexDirection: "column",
-        justifyContent: "flex-start"
-      },
-      text: {
-        fontSize: 20,
-        textAlign: "center",
-      },
-      textContainer: {
-        flex: 1,
-        flexGrow: 0,
-        flexShrink: 1,
-        justifyContent: "center",
-        marginHorizontal: 20,
-        marginVertical: 45,
-      },
-      button: {
-        flexGrow: 0,
-        flexShrink: 1,
-        flexBasis: 80,
-        alignItems: "center",
-        justifyContent: "center",
-        marginHorizontal: "10%",
-        backgroundColor: TERTIARY,
-        borderWidth: 1,
-        borderRadius: 20,
-        marginBottom: 10
-      },
-      buttonText: {
-        textAlign: "center",
-      },
+        justifyContent: "flex-start",
+    },
+    text: {
+      flex:1,
+      flexWrap: "wrap",
+      fontSize: 20,
+      textAlign: "center",
+    },
+    textContainer: {
+      flex: 1,
+      flexGrow: 0,
+      flexShrink: 1,
+      alignItems: "center",
+      justifyContent: "center",
+      marginHorizontal: 25,
+      marginVertical: 50,
+    },
+    motivatorContainer: {
+      flex: 1,
+      flexShrink: 1,
+      flexGrow: 0,
+      marginHorizontal: 25,
+      flexDirection: "row",
+      justifyContent: "center",
+      alignItems: "flex-start",
+      alignSelf: "center",
+    },
+    arrowContainer: {
+      flex: 1,
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    button: {
+      flexGrow: 0,
+      flexShrink: 1,
+      flexBasis: 80,
+      alignItems: "center",
+      justifyContent: "center",
+      marginHorizontal: "10%",
+      backgroundColor: TERTIARY,
+      borderWidth: 1,
+      borderRadius: 20,
+      marginBottom: 10
+    },
+    buttonText: {
+      textAlign: "center",
+    },
 });
