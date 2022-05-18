564edb948483eb0feffe2f2aec9eef384045fc68
Simon Doba <simon.doba@hotmail.de>
2022-05-19 00:49:32 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -93,7 +93,7 @@ module.exports = {
     'space-before-function-paren': [
       'error',
       {
-        anonymous: 'never',
+        anonymous: 'always',
         named: 'never',
         asyncArrow: 'always',
       },
