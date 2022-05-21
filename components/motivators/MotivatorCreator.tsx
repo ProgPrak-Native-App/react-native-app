80d2c77e968010e4b923aa5f813c82ae459a0e69
Jonah Balshai <jonahbalshai@gmail.com>
2022-05-21 16:58:25 +0200

    refining touches

--- components/motivators/MotivatorCreator.tsx
+++ components/motivators/MotivatorCreator.tsx
@@ -4,10 +4,8 @@ import { TERTIARY, MOTIVATOR } from "../../colors";
 import { AntDesign } from "@expo/vector-icons";
 import Motivator from "./Motivator";
 import Title from "../Title";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function CreateMotivator() {
-    console.log("TEST");
     return (
       <>
         <Title text="Neue Starkmacher"/>
@@ -20,11 +18,18 @@ export default function CreateMotivator() {
               <AntDesign name="left" size={48} />
             </View>
             <Motivator icon={require("../../assets/reframeicon.png")} title="Reframing" description="Beim Reframing geht es darum, deine eigene Einschätzung der Situation zu überprüfen und ggf. zu einer anderen Interpretation zu kommen."/>
-            <View style={styles.arrowContainer}>
-              <AntDesign name="right" size={48}/>
-            </View>
+            <Pressable>
+              <View style={styles.arrowContainer}>
+                <AntDesign name="right" size={48}/>
+              </View>
+            </Pressable>
           </View>
         </View>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>
+            Let's go!
+          </Text>
+        </Pressable>
       </>
     );
 }
@@ -36,47 +41,39 @@ const styles = StyleSheet.create({
         justifyContent: "flex-start",
     },
     text: {
-      flex:1,
-      flexWrap: "wrap",
       fontSize: 20,
       textAlign: "center",
     },
     textContainer: {
       flex: 1,
-      flexGrow: 0,
-      flexShrink: 1,
       alignItems: "center",
       justifyContent: "center",
       marginHorizontal: 25,
-      marginVertical: 50,
     },
     motivatorContainer: {
-      flex: 1,
       flexShrink: 1,
       flexGrow: 0,
-      marginHorizontal: 25,
       flexDirection: "row",
       justifyContent: "center",
-      alignItems: "flex-start",
+      alignItems: "center",
       alignSelf: "center",
     },
     arrowContainer: {
-      flex: 1,
       flexDirection: "column",
       alignItems: "center",
       justifyContent: "center",
     },
     button: {
-      flexGrow: 0,
+      flexGrow: 1/16,
       flexShrink: 1,
-      flexBasis: 80,
       alignItems: "center",
       justifyContent: "center",
       marginHorizontal: "10%",
       backgroundColor: TERTIARY,
       borderWidth: 1,
       borderRadius: 20,
-      marginBottom: 10
+      marginBottom: 10,
+      marginTop: 5,
     },
     buttonText: {
       textAlign: "center",
