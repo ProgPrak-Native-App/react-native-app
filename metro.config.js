320206b99d6ec9e00e8a9e7d8c3888b991f5b3ca
redstarmy <58826939+redstarmy@users.noreply.github.com>
2022-06-17 14:23:12 +0200

    ignore metro config file

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,4 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-import { getDefaultConfig } from 'expo/metro-config';
+const {getDefaultConfig} = require('expo/metro-config');
 
 module.exports = getDefaultConfig(__dirname);
