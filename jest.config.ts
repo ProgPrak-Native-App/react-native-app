a6c77584839f58752c5dcac40b8b628896f3ab4e
Simon Doba <simon.dp.doba@campus.tu-berlin.de>
2022-07-05 22:45:36 +0200

    [Updated | #20] Jest config and CI workflow to run unit test on build

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from '@jest/types';
+
 const untranspiledModulePatterns = [
-  '(jest-)?react-native',
+  '(jest-)?@?react-native',
   '@react-native-community',
   'expo(nent)?',
   '@expo(nent)?/.*',
@@ -11,15 +12,30 @@ const untranspiledModulePatterns = [
   'sentry-expo',
   'native-base',
   'react-native-svg',
+  'react-native-calendars',
+  'react-native-codegen',
+  'react-native-countdown-component',
+  'react-native-fit-image',
+  'react-native-get-random-values',
+  'react-native-gradle-plugin',
+  'react-native-safe-area-context',
+  'react-native-screens',
+  'react-native-sortable-list',
+  'react-native-swipe-gestures',
+  'react-native-web',
 ];
 
 const config: Config.InitialOptions = {
   preset: 'jest-expo',
+  cacheDirectory: '.cache/jest',
+  coverageDirectory: './coverage',
   testEnvironment: 'node',
   verbose: true,
   automock: false,
+  reporters: ['default', 'github-actions'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   testMatch: ['**/?(*.)+(spec|test).ts?(x)'],
+  collectCoverage: true,
   collectCoverageFrom: [
     '**/*.{ts,tsx}',
     '!**/coverage/**',
@@ -28,10 +44,7 @@ const config: Config.InitialOptions = {
     '!**/jest.setup.js',
   ],
   transformIgnorePatterns: [`node_modules/(?!${untranspiledModulePatterns.join('|')})`],
-  transform: {
-    '^.+\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js',
-    '^.+\\.tsx?$': 'ts-jest',
-  },
+  transform: {},
   coverageReporters: ['json-summary', 'text', 'lcov'],
 };
 
