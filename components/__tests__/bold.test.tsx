5356273cbbe83c56c1f1ed7bbc8025260540d621
Simon Doba <simon.dp.doba@campus.tu-berlin.de>
2022-07-05 22:48:52 +0200

    [Updated | #20] Unit test for Bold component

--- components/__tests__/bold.test.tsx
+++ components/__tests__/bold.test.tsx
@@ -4,29 +4,8 @@ import renderer from 'react-test-renderer';
 import Bold from '../Bold';
 
 describe('<Bold />', () => {
-  it('Is a Bold component', async () => {
-    const tree = renderer.create(<Bold />);
-    expect(tree.getInstance()?.type).toBe(<Bold />);
-  });
   it('Matches Snapshot', async () => {
     const tree = renderer.create(<Bold />).toJSON();
     expect(tree).toMatchSnapshot();
   });
-  it('Text is bold', async () => {
-    const tree = renderer.create(<Bold />);
-    console.log('tree root', tree.root);
-    console.log('tree toTree', tree.toTree());
-
-    const styles = tree.getInstance();
-    console.log('styles', styles);
-    expect(styles).toBe('bold');
-  });
-
-  it('Adding additional property works', async () => {
-    // eslint-disable-next-line react-native/no-raw-text
-    const tree = renderer.create(<Bold style={{ backgroundColor: 'red' }}>Boldtext</Bold>).toJSON();
-    console.log('tree as json', tree);
-    expect(tree).toMatchSnapshot();
-  });
-  afterAll(() => setTimeout(() => process.exit(0), 1000));
 });
