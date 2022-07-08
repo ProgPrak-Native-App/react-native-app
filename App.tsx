43a4ce649828cbc59f4d240581eb9f6ab5ac4426
redstarmy <58826939+redstarmy@users.noreply.github.com>
2022-07-08 14:19:12 +0200

    layout: revert to react-native-safe-area-context

--- App.tsx
+++ App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import AppWrapper from './components/AppWrapper';
 import { UserProvider } from './components/UserProvider';
 import { BACKGROUND } from './styles';
-import SafeAreaView from 'react-native-safe-area-view';
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <SafeAreaView forceInset={{ bottom: 'never', top: 'never' }} style={{ flex: 1, backgroundColor: BACKGROUND }}>
+      <SafeAreaView edges={['right', 'left']} style={{ flex: 1, backgroundColor: BACKGROUND }}>
         <UserProvider>
           <AppWrapper />
         </UserProvider>
