d47269d41d5df9c2174c103e7c881a3b576cfddd
Simon Doba <simon.dp.doba@campus.tu-berlin.de>
2022-07-08 15:23:05 +0200

    [Updated | #8] BaseRestClient

--- api/BaseClient.ts
+++ api/BaseClient.ts
@@ -1,11 +1,44 @@
 export default class BaseClient {
   constructor(protected baseUrl: URL | string) {}
 
-  protected async get<R>(path: string): Promise<R> {
-    const response = await fetch(new URL(path, this.baseUrl), {
-      method: 'GET',
-    });
+  protected async get<R>(path: string, options?: RequestInit): Promise<R> {
+    if (options) {
+      Object.assign(options, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    }
+
+    const response = await fetch(new URL(path, this.baseUrl), options);
 
     return (await response.json()) as R;
+  }
+
+  protected async post(path: string, body: string | null, options?: RequestInit): Promise<Response> {
+    if (options) {
+      Object.assign(options, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    }
+
+    return await fetch(new URL(path, this.baseUrl), { ...options, body: JSON.stringify(body) });
+  }
+
+  protected async remove(path: string, options?: RequestInit): Promise<Response> {
+    if (options) {
+      Object.assign(options, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    }
+
+    return await fetch(new URL(path, this.baseUrl), options);
   }
 }
