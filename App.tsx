d7065ebed555bebeacad84ac9152699027cc3a21
redstarmy <58826939+redstarmy@users.noreply.github.com>
2022-07-08 14:02:44 +0200

    layout: removed bottom and top inset from safeView

--- App.tsx
+++ App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AppWrapper from './components/AppWrapper';
 import { UserProvider } from './components/UserProvider';
 import { BACKGROUND } from './styles';
+import SafeAreaView from 'react-native-safe-area-view';
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1, backgroundColor: BACKGROUND }}>
+      <SafeAreaView forceInset={{ bottom: 'never', top: 'never' }} style={{ flex: 1, backgroundColor: BACKGROUND }}>
         <UserProvider>
           <AppWrapper />
         </UserProvider>
