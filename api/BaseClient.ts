17aea10361c0a3ed9b191668e62664a40cee6c66
Simon Doba <simon.dp.doba@campus.tu-berlin.de>
2022-08-01 16:33:18 +0200

    [Change | #25] Migrated app.json to dynamic app.config file, Updated: BaseClient to be aware of environment, added api services for selection

--- api/BaseClient.ts
+++ api/BaseClient.ts
@@ -1,7 +1,14 @@
+import Constants from 'expo-constants';
 export default class BaseClient {
-  constructor(protected baseUrl: URL | string) {}
+  // Default environment is dev if not otherwise specified for NODE_ENV on runtime
+  // Available environments: [live, dev, stage]
+  private baseUrl: string;
 
-  protected async get<R>(path: string): Promise<R> {
+  constructor(protected serviceName: URL | string) {
+    this.baseUrl = `https://${serviceName}.api.${Constants?.manifest?.extra?.environment}.mindtastic.lol`;
+  }
+
+  async get<R>(path: string): Promise<R> {
     const response = await fetch(new URL(path, this.baseUrl), {
       method: 'GET',
     });
--- app.config.js
+++ app.config.js
@@ -1,10 +1,40 @@
 // eslint-disable-next-line import/no-anonymous-default-export
-export default ({ config }) => {
+import { version } from './package.json';
+
+export default ({}) => {
   return {
-    ...config,
-    version: process.env.APP_VERSION || '1.0.0',
+    owner: 'progprak_kopfsachen',
+    name: 'Kopfsachen-Native-App',
+    slug: 'kopfsachen',
+    orientation: 'portrait',
+    icon: './assets/icon.png',
+    splash: {
+      image: './assets/splash.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff',
+    },
+    updates: {
+      fallbackToCacheTimeout: 0,
+    },
+    assetBundlePatterns: ['**/*'],
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: 'org.kopfsachen.app',
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/adaptive-icon.png',
+        backgroundColor: '#CAE2E2',
+      },
+      package: 'org.kopfsachen.app',
+    },
+    web: {
+      favicon: './assets/favicon.png',
+    },
+    version: version || '1.0.0',
     extra: {
-      environment: process.env.NODE_ENV ?? 'development',
+      // Available environments: live, dev, stage
+      environment: process.env.NODE_ENV ?? 'dev',
     },
   };
 };
